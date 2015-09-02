27051242186311895f233096072bd81cade07ff5
kakl <kakl@astro.cz>
2015-09-02 20:27:20 +0930

    calibrated version

--- ph.py
+++ ph.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 """
-Testbed for the animation.
-
-It basically produces series of temperatures that get animated on the client
-browser side.
+pH meter
 
 """
 
@@ -17,11 +14,15 @@ from pylab import *
 import time
 import numpy as np
 
-t = [1 + i for i in range(0, 70)]
-s = [0]*70
-s2 = [0]*70
-s[69] = 40
-s2[69] = 14
+S = 0.0569    # V/pH    
+pHoffset = 0.022 # V (offset for pH = 7)
+points = 100
+
+t = [1 + i for i in range(-points, 0)]
+s = [0]*points
+s2 = [0]*points
+s[points-1] = 40
+s2[points-1] = 14
 
 plot(t, s2, linewidth=3.0)
 xlabel('time')
@@ -64,7 +65,7 @@ while True:
     cfg.initialize()
     adc = cfg.get_device("adc")
 
-    time.sleep(0.5)
+    time.sleep(0.1)
 
     n =  t[-1] + 1
     try:
@@ -76,12 +77,13 @@ while True:
             #temperature -= 1.5
             print "Internal Temperature =", float("{0:.2f}".format(temperature))
            
-            time.sleep(1)
+            time.sleep(59.6)
 
             # Voltage readout
             voltage = adc.readADC()
-            ph = -(voltage / 0.050) + 7   #pH
-            print "Voltage =", voltage, ",  pH =", float("{0:.2f}".format(ph))
+            #ph = 7 - (voltage - 0.022) / S   #pH
+            ph = voltage * (-17.75) + 7.58
+	    print "Voltage =", voltage, ",  pH =", float("{0:.2f}".format(ph))
 
             # refresh graph
             s = s[1:] + [ph]
@@ -91,10 +93,10 @@ while True:
             if (n % 10) == 0:
                 ax.text(n, ph-1, "{0:.2f}".format(ph),
                 verticalalignment='bottom', horizontalalignment='right',
-                color='red', fontsize=16)
+                color='red', fontsize=11)
                 ax2.text(n, temperature-5, "{0:.1f}".format(temperature),
                 verticalalignment='bottom', horizontalalignment='right',
-                color='green', fontsize=16)
+                color='green', fontsize=11)
 
             ax.lines[0].set_xdata(t)
             ax.lines[0].set_ydata(s)
@@ -104,8 +106,6 @@ while True:
             ax2.set_xlim(t[0],t[-1])
             f.canvas.draw()
             f2.canvas.draw()
-            #time.sleep(0.5)
-
 
 
 
